bb1a4c656011298cf8d0ea92e91d128389aee2d1
Avik Kundu <1828008@kiit.ac.in>
2023-01-02 04:34:15 +0530

    update

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@ TAG=$1
 NAMESPACE=$2
 CHART_NAME=$3
 ACR_LOGIN_SERVER=$4
-ACR_REGISTRY_NAME=$5
+echo "oci://$ACR_LOGIN_SERVER/helm/$CHART_NAME"
 export KUBECONFIG=$HOME/.kube/config
 result=$(eval helm ls --namespace $NAMESPACE | grep $CHART_NAME)
 if [ $? -ne "0" ]; then
    echo "Installing Helm Chart"
-   helm install --namespace $NAMESPACE --create-namespace $CHART_NAME "oci://$ACR_LOGIN_SERVER/$ACR_REGISTRY_NAME" --version $TAG
+   helm install --namespace $NAMESPACE --create-namespace $CHART_NAME "oci://$ACR_LOGIN_SERVER/helm/$CHART_NAME" --version $TAG
 else
    echo "Upgrading Helm Chart"
-   helm upgrade --namespace $NAMESPACE $CHART_NAME "oci://$ACR_LOGIN_SERVER/$ACR_REGISTRY_NAME" --version $TAG
+   helm upgrade --namespace $NAMESPACE $CHART_NAME "oci://$ACR_LOGIN_SERVER/helm/$CHART_NAME" --version $TAG
 fi
